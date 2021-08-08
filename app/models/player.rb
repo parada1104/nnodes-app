aa3f5e76bdc06e221bd4fdfea262637e00374054
robert parada <parada11.04@gmail.com>
2021-08-08 19:22:33 -0400

    player model setup(passing initials tests)

--- app/models/player.rb
+++ app/models/player.rb
@@ -11,4 +11,15 @@
 #  updated_at       :datetime         not null
 #
 class Player < ApplicationRecord
+  ##associations
+  has_many :bets , dependent: :destroy
+  has_many :rounds, through: :bets
+  accepts_nested_attributes_for :bets
+
+  ##monetize
+  monetize :balance_cents, as: :balance
+
+  ##validations
+  validates :name, presence: true
+  validates :lastName, presence: true
 end
