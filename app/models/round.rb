36bfbce71876fb6271eb23cd31fcb8db0eb1e021
robert parada <parada11.04@gmail.com>
2021-08-08 20:11:32 -0400

    round model setup bug: to resolve validation on enum inclusion for color
    Signed-off-by: robert parada <parada11.04@gmail.com>

--- app/models/round.rb
+++ app/models/round.rb
@@ -8,4 +8,12 @@
 #  updated_at :datetime         not null
 #
 class Round < ApplicationRecord
+  enum color: { green: 0, red: 1, black: 2 }
+
+  ##associations
+  has_many :bets, dependent: :destroy
+  has_many :players, through: :bets
+
+  ##validations
+  validates :color, presence: true, inclusion: { in: Round.colors.keys }
 end
